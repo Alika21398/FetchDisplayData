0183a0e69d88e807d03d90ff4a6bb1e80f813712
Alika <dangolalika2017@gmail.com>
2024-11-09 20:40:17 +0545

    updated App.jsx and Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ export function useApi() {
 const App = () => {
   const [data, setData] = useState([]);
   const [filteredPost, setFilteredPost] = useState([]);
+  // to store userId that comes from Home.jsx
+  const [selectedUserData, setSelectedUserData] = useState([]);
 
   const API = "https://jsonplaceholder.typicode.com/posts";
 
@@ -30,11 +32,39 @@ const App = () => {
   useEffect(() => {
     apiData();
   }, []);
+
+  // function that handles data from home component to app
+
+  const handleUserData = (userId) => {
+    return setSelectedUserData(userId);
+  };
+
+  // filters entire that to display those data that has userId which is sent from home
+  const displayedPost = data.filter((post) => post.userId == selectedUserData);
+
   console.log("data", data);
   console.log("filteredPost", filteredPost);
+  console.log("selectedUserData", selectedUserData);
+
   return (
-    <ApiContext.Provider value={{ data, filteredPost }}>
+    <ApiContext.Provider value={{ data, filteredPost, handleUserData }}>
       <Outlet />
+      <div className=" text-center border border-b-2 ">
+        {/* checking length of data displaying so that if there is no data yet fetched the sentence "Data with userId" is not displayed */}
+        {displayedPost.length > 0 && (
+          <div className="text-lg font-bold ">
+            Data with userId {selectedUserData}
+          </div>
+        )}
+
+        {displayedPost.map((items, index) => {
+          return (
+            <div key={index}>
+              <div className="my-5"> {items.title}</div>
+            </div>
+          );
+        })}
+      </div>
     </ApiContext.Provider>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { useApi } from "../App";
 
 const Home = () => {
-  const { data, filteredPost } = useApi();
+  const { filteredPost, handleUserData } = useApi();
+  const [value, setValue] = useState(0);
   return (
     <>
       <div className="px-44 my-10">
         <div className="my-10  text-center">
           <h1 className="font-bold text-4xl">Welcome </h1>
-        
-        <h2 className="text-2xl mt-5">Click the buttons below</h2>
         </div>
 
-
-        <div className="flex justify-between">
-            <button>UserId</button>
-            <button>UserId</button>
-            <button>UserId</button>
+        <div>
+          {filteredPost.slice(0, 3).map((item, index) => {
+            return (
+              <div key={index}>
+                <div className="my-10 border-y-2"></div>
+                <div>UserId:{item.userId}</div>
+                <div className="">Title:{item.title}</div>
+                <div>Body:{item.body}</div>
+              </div>
+            );
+          })}
         </div>
-        {filteredPost.map((item, index) => {
-          return <div key={index}>{item.title}</div>;
-        })}
+        <div className="text-center">
+          <div>
+            <h2 className="text-2xl mt-5 font-bold">Click the buttons below</h2>
+          </div>
+
+          <div className="flex justify-between my-5">
+            {/* onclick function handleuserData is being used as it sends numbers that represents userId which is compared using filter in App.jsx DisplayedPost  to display datas */}
+            {/* value and setValue states are used here to chnage css making using ternary operation */}
+            <button
+              onClick={() => {
+                handleUserData(1);
+                setValue(1);
+              }}
+              style={{
+                backgroundColor: value == 1 ? "cyan" : "black",
+                color: value == 1 ? "black" : "white",
+              }}
+              className="px-4 py-2 bg-black text-white border-2"
+            >
+              UserId 1
+            </button>
+            <button
+              onClick={() => {
+                handleUserData(2);
+                setValue(2);
+              }}
+              style={{
+                backgroundColor: value == 2 ? "cyan" : "black",
+                color: value == 2 ? "black" : "white",
+              }}
+              className="px-4 py-2 bg-black text-white border-2"
+            >
+              UserId 2
+            </button>
+            <button
+              onClick={() => {
+                handleUserData(3);
+                setValue(3);
+              }}
+              style={{
+                backgroundColor: value == 3 ? "cyan" : "black",
+                color: value == 3 ? "black" : "white",
+              }}
+              className="px-4 py-2 bg-black text-white border-2"
+            >
+              UserId 3
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
